748e4cc9cd4dc275cc2f8f41a2d3e3379e1fd7b4
Ruth Chisom <ndukwe100805043@gmail.com>
2022-09-08 13:10:32 +0100

    Home and About Pages

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+  assetsURL = environment.assetsURL;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
